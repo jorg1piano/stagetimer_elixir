69d08a35850bf8828a6638564362e6b19351db61
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-16 11:27:09 +0200

    chore: scaffold initial Elixir mix project

---
+++ lib/stagetimer.ex
@@ -0,0 +1,18 @@
+defmodule Stagetimer do
+  @moduledoc """
+  Documentation for `Stagetimer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stagetimer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stagetimer_test.exs
@@ -0,0 +1,8 @@
+defmodule StagetimerTest do
+  use ExUnit.Case
+  doctest Stagetimer
+
+  test "greets the world" do
+    assert Stagetimer.hello() == :world
+  end
+end
