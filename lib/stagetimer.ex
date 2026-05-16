1f2745376785d82f43525bd58f0f7c2dac64ded0
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-16 12:04:56 +0200

    chore: remove placeholder hello world module docs and test generated by mix

--- lib/stagetimer.ex
+++ lib/stagetimer.ex
@@ -1,17 +1,4 @@
 defmodule Stagetimer do
-  @moduledoc """
-  Documentation for `Stagetimer`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Stagetimer.hello()
-      :world
-
-  """
   def hello do
     :world
   end
--- test/stagetimer_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule StagetimerTest do
-  use ExUnit.Case
-  doctest Stagetimer
-
-  test "greets the world" do
-    assert Stagetimer.hello() == :world
-  end
-end
